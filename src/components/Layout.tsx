19b920c072d753ec3c1312c25f9d64665d353015
Amit-Shaw7 <maahiamit777@gmail.com>
2024-02-14 01:54:01 +0530

    added profile page link

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import Container from "./Container";
 
 export default function Layout() {
     return (
         <div className="h-[100vh] flex items-center justify-center">
             <div className="h-[700px] w-[375px] bg-gray-light">
+                <div className="absolute top-[68px] left-[15px]">
+                    <Link to="/profile">
+                        View the profile page form <span className="text-violet underline">here</span>
+                    </Link>
+                </div>
                 <Container>
                     <Outlet />
                 </Container>
